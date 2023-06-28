8c61ae9cd028be668954bf34432a339402848060
Adwait Srivastava <studyuse1702@gmail.com>
2023-06-28 21:47:33 +0530

    Working on additional feaures of the courses

--- Assessment.py
+++ Assessment.py
@@ -1,8 +1,11 @@
 class Assessment:
-    def __init__(self,assessment_name,weight = "Undefined",mark = 0.0):
+    def __init__(self,assessment_name,freq=0, drops=0, each_weight=0, weight = "Undefined",mark = 0.0,):
         self.name = assessment_name
         self.weight = weight
         self.mark = mark
+        self.freq=freq
+        self.drops=drops
+        self.each_weight=each_weight
 
     def __eq__(self, obj):
         return isinstance(obj, Assessment) and obj.name == self.name
@@ -11,3 +14,10 @@ class Assessment:
     
     def update_weight(self,weight):
         self.weight = weight
+    def update_freq(self,freq):
+        self.freq=freq
+    def update_drops(self,drops):
+        self.drops=drops
+    def update_each_weight(self,each_weight):
+        self.each_weight= each_weight
+    
--- Course.py
+++ Course.py
@@ -31,9 +31,11 @@ class Course:
         print("0. Exit")
         for num, key in enumerate(menu.keys()):
             print(str(num + 1) + '. ' + menu[key])
-        print("Or enter any other assessment titles directly\nAssessment: ", end="")
+        print("Or enter any other assessment titles directly ", end="")
+        count=0
         while True:
-            a = input()
+            
+            a = input("Enter assessment Choice {0}:".format((count+1)))
             if a == '0':
                 break
             elif a in menu:
@@ -41,25 +43,44 @@ class Course:
             else:
                 self.assessments_dict.setdefault(a, Assessment(a))
         print("Course Assessment Titles Entry Complete.")
+    def trial_func(self,assessment_obj,valid_input):
+        
+        while not valid_input:
+                try:
+                    weight = float(input("Enter Net weightage for " + assessment_obj.name + ": "))
+                    freq=int(input("Enter number of times this assessment is testsed:"))
+                    each_weight=float(input("Enter weightage of each {0} ".format(assessment_obj.name)))
+                    drops=int(input("Enter number of assessments dropped:"))
+                    if weight < 0 or freq<0 or each_weight<0 or drops<0:
+                        print("No attribute for an assessment can be negative!")
+                        
+                    else:
+                        if drops<=freq and ((freq-drops)*each_weight)==weight:
+                            assessment_obj.update_weight(weight)
+                            assessment_obj.update_freq(freq)
+                            assessment_obj.update_each_weight(each_weight)
+                            assessment_obj.update_drops(drops)
+                            valid_input = True
+
+                        else:
+                            print("Attributes entered incorrectly please try again")
+                            
+                except ValueError:
+                    print("You did not enter a number!")
+
+    def assessment_parameters(self):
+        valid_input=False
+        for assessment_obj in self.assessments_dict.values():
+            valid_input = False
+            if assessment_obj.weight != 'Undefined':
+                valid_input = True
+            self.trial_func(assessment_obj,valid_input)
 
     def initiate_distribution(self):
         self.add_assessment()
         # Course Distribution
         print("Please enter the weight for the assessments entered: ")
-        for assessment_obj in self.assessments_dict.values():
-            valid_input = False
-            if assessment_obj.weight != 'Undefined':
-                valid_input = True
-            while not valid_input:
-                try:
-                    weight = float(input("Enter weight for " + assessment_obj.name + ": "))
-                    if weight < 0:
-                        print("Weight for an assessment cannot be negative!")
-                    else:
-                        assessment_obj.update_weight(weight)
-                        valid_input = True
-                except ValueError:
-                    print("You did not enter a number!")
+        self.assessment_parameters()
         print("Course Assessment Weighting Entry Complete.")
 
     def edit_or_modify_assessment(self):
@@ -73,7 +94,7 @@ class Course:
                 self.initiate_distribution()
             elif name in self.assessments_dict.keys():
                 print(name, "is selected.")
-                ans = input("Do you wish to delete this assessment (1) or change its weight (2)? ")
+                ans = input("Do you wish to delete this assessment (1) or change its grade distribution (2)? ")
                 while not (ans == '0' or ans == '1' or ans == '2'):
                     ans = input("Invalid Input. Please only enter '1' or '2'.\
                     \nDo you wish to delete this assessment (1) or change its weight (2)? ")
@@ -84,17 +105,7 @@ class Course:
                     self.assessments_dict.pop(name)
                 elif ans == '2':
                     assess = self.assessments_dict.get(name)
-                    valid_input = False
-                    while not valid_input:
-                        try:
-                            new_weight = float(input("Enter weight for " + assess.name + ": "))
-                            if new_weight < 0:
-                                print("Weight for an assessment cannot be negative!")
-                            else:
-                                self.assessments_dict.get(name).update_weight(new_weight)
-                                valid_input = True
-                        except ValueError:
-                            print("You did not enter a number!")
+                    self.trial_func(assess,valid_input=False)
             else:
                 print("'" + name + "' is not in this course assessment list, please try again.")
 
